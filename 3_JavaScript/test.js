11c95698350807740c50f6f8de8cb8324e89e2bf
shanwei <shanwei@yeah.net>
2021-01-10 17:06:51 +0800

    javascript

---
+++ 3_JavaScript/test.js
@@ -0,0 +1,19 @@
+document.getElementById("btn1").onclick = function () 
+{
+  console.log("haha");
+};
+
+/**
+ * 这是文档注释的例子
+ * 这是一个演示用的函数
+ * @param name 这是姓名
+ * @param age 这是年龄
+ * @returns 一个字符串
+ */
+// function test(name, age) {
+//     return 'xxx';
+// }
+
+"use strict"
+
+var x = 5;
