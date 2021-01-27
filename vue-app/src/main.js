2d7e84e1ada677ebec97fb736d7f2f78abeee58e
Sunjingwen <453129077@qq.com>
2021-01-27 17:00:48 +0800

    book

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -9,8 +9,11 @@ import './assets/js/rem.js'
 // 引入第三方库
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(VueAxios, axios)
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
